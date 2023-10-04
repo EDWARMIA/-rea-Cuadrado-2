678ab334d5299797f1c8f1d288ce54683198b732
EDWAR <EDWAR@EDWAR>
2023-10-04 07:12:07 -0500

    Agregar archivos de proyecto.

---
+++ Área Cuadrado/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Área_Cuadrado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Calculadora de área de cuadrado");
+
+                Console.Write("Ingrese la longitud de un lado del cuadrado: ");
+                double lado = double.Parse(Console.ReadLine());
+
+                double area = lado * lado;
+
+                Console.WriteLine($"El área del cuadrado con lado {lado} es: {area}");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
